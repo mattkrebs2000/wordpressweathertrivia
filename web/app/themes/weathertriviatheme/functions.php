ff617f12639e4bb1525a7e51a66d8867b8c32ed5
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-08-18 15:38:24 -0400

    new change

--- web/app/themes/weathertriviatheme/functions.php
+++ web/app/themes/weathertriviatheme/functions.php
@@ -358,17 +358,12 @@ jQuery(document).ready(function() {
         jQuery("#begin").addClass("bottom2");
     });
 });
-
-
-
-function remove_admin_login_header() {
-    remove_action('wp_head', '_admin_bar_bump_cb');
-}
-add_action('get_header', 'remove_admin_login_header');
 </script>
 <?php
 }
+function remove_admin_login_header() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+}
 
-
-
+add_action('get_header', 'remove_admin_login_header');
 add_action('wp_head','add_js_functions');
